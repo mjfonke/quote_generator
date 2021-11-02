3fe5c1d021d37076ecd56673406e020a68572060
Miran Jang <miranjang9507@gmail.com>
2021-11-01 21:08:14 -0400

    added loader

--- app.js
+++ app.js
@@ -3,12 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 // Get Quotes From API
 let apiQuotes = [];
 
+// show loading
+function loading() {
+    loader.hidden = false;
+    quoteBox.hidden = true;
+}
+
+// hide Loading
+function complete() {
+    quoteBox.hidden = false;
+    loader.hidden = true;
+}
+
 // generate new quote
 function newQuote() {
+    loading();
     const randNum = Math.floor(Math.random() * apiQuotes.length);
     const quote = apiQuotes[randNum];
     if (!quote.author) {
@@ -22,20 +36,25 @@ function newQuote() {
         quoteText.classList.remove('long-quote');
     }
     // console.log(quote);
+    // set Quote, Hide Loader
     quoteText.textContent = quote.text;
-
+    complete();
 };
 
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
         newQuote();
+
     } catch (err) {
         // handling errors
 
     }
+
+    complete();
 };
 
 // Tweet Quote
